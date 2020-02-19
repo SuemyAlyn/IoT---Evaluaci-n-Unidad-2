efd978077f6ee52a9fa47ab3e143af1ed91de5a5
SuemyAlyn <48026508+SuemyAlyn@users.noreply.github.com>
2020-02-19 14:03:15 -0600

    Add files via upload

---
+++ raiz_cuadrada.py
@@ -0,0 +1,40 @@
+# ejercicio_cinco
+# raiz_cuadrada.py
+
+import math
+
+def raiz_cuadrada(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10):
+      r1 = math.sqrt(n1)
+      r2 = math.sqrt(n2)
+      r3 = math.sqrt(n3)
+      r4 = math.sqrt(n4)
+      r5 = math.sqrt(n5)
+      r6 = math.sqrt(n6)
+      r7 = math.sqrt(n7)
+      r8 = math.sqrt(n8)
+      r9 = math.sqrt(n9)
+      r10 = math.sqrt(n10)
+      print ('\n' + "Las raíces son las siguientes: " + '\n')
+      print (f"La raiz del número {n1} es {r1}")
+      print (f"La raiz del número {n2} es {r2}")
+      print (f"La raiz del número {n3} es {r3}")
+      print (f"La raiz del número {n4} es {r4}")
+      print (f"La raiz del número {n5} es {r5}")
+      print (f"La raiz del número {n6} es {r6}")
+      print (f"La raiz del número {n7} es {r7}")
+      print (f"La raiz del número {n8} es {r8}")
+      print (f"La raiz del número {n9} es {r9}")
+      print (f"La raiz del número {n10} es {r10}")
+
+na = int(input("Teclea el valor 1: "))
+nb = int(input("Teclea el valor 2: "))
+nc = int(input("Teclea el valor 3 : "))
+nd = int(input("Teclea el valor 4: "))
+ne = int(input("Teclea el valor 5: "))
+nf = int(input("Teclea el valor 6: "))
+ng = int(input("Teclea el valor 7: "))
+nh = int(input("Teclea el valor 8: "))
+ni = int(input("Teclea el valor 9: "))
+nj = int(input("Teclea el valor 10: "))
+
+raiz_cuadrada(na,nb,nc,nd,ne,nf,ng,nh,ni,nj)
---
+++ tablas_multiplicar.py
@@ -0,0 +1,58 @@
+# ejercicio_seis
+# Tablas de multiplicar
+
+def tablas_mult(res):
+      
+      print('\n'+"Tabla del 1")
+      for i in range(11):
+            m = i * 1
+            print(f"1 x {i} = {m}")
+
+      print('\n'+"Tabla del 2")
+      for i in range(11):
+            m = i * 2
+            print(f"2 x {i} = {m}")
+
+      print('\n'+"Tabla del 3")
+      for i in range(11):
+            m = i * 3
+            print(f"3 x {i} = {m}")
+
+      print('\n'+"Tabla del 4")
+      for i in range(11):
+            m = i * 4
+            print(f"4 x {i} = {m}")
+
+      print('\n'+"Tabla del 5")
+      for i in range(11):
+            m = i * 5
+            print(f"5 x {i} = {m}")
+
+      print('\n'+"Tabla del 6")
+      for i in range(11):
+            m = i * 6
+            print(f"6 x {i} = {m}")
+
+      print('\n'+"Tabla del 7")
+      for i in range(11):
+            m = i * 7
+            print(f"7 x {i} = {m}")
+
+      print('\n'+"Tabla del 8")
+      for i in range(11):
+            m = i * 8
+            print(f"8 x {i} = {m}")
+
+      print('\n'+"Tabla del 9")
+      for i in range(11):
+            m = i * 9
+            print(f"9 x {i} = {m}")
+
+      print('\n'+"Tabla del 10")
+      for i in range(11):
+            m = i * 10
+            print(f"10 x {i} = {m}")
+        
+
+r = int(input("Imprimo las tablas de multiplicar para ti? 1 = Si, 2 = No "))
+tablas_mult(r)
